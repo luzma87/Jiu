e4479341a26a3cb13a4142c6e913e6ea900b0cf2
Luz <luzma_87@yahoo.com>
2018-06-17 00:07:18 -0500

    adds students navigation

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withTheme } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Group from '@material-ui/icons/Group';
 
 function TopBar(props) {
   return (
     <div>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="title" color="inherit" >
-            Gracie Jiu-Jitsu Quito
-          </Typography>
+          <Button
+            component={Link}
+            to="/"
+            color="inherit"
+          >
+            <Typography
+              variant="title"
+              color="inherit"
+            >
+              Gracie Jiu-Jitsu Quito
+            </Typography>
+          </Button>
+          <Button
+            component={Link}
+            to="/studentForm"
+            color="inherit"
+          >
+            <Group className="leftIcon" />
+            Estudiantes
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
