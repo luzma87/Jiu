e4b20ad064d0862ba9c20b7cf10da5bad60147c0
Luz <luzma_87@yahoo.com>
2018-07-28 16:03:18 -0500

    adds photos to home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 import { withTheme } from '@material-ui/core/styles';
 
-function App(props) {
+const classes = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    paddingTop: 40,
+  },
+  gridList: {
+    width: 400,
+    height: 300,
+  },
+};
+
+const images = [
+  {
+    img: '../assets/photos/1.png',
+    cols: 1,
+  },
+  {
+    img: '../assets/photos/2.png',
+    cols: 1,
+  },
+  {
+    img: '../assets/photos/3.png',
+    cols: 1,
+  },
+  {
+    img: '../assets/photos/4.png',
+    cols: 1,
+  },
+];
+
+function App() {
   return (
-    <div>
-      HOLA!
+    <div style={classes.root}>
+      <GridList
+        cellHeight={160}
+        style={classes.gridList}
+        cols={2}
+      >
+        {images.map(tile => (
+          <GridListTile
+            key={tile.img}
+            cols={tile.cols || 1}
+          >
+            <img src={tile.img} />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 }
